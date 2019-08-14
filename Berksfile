8b91b8fb4a98bfcea9558528fa79dacc4a72ee4d
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-08-14 09:18:06 +0100

    Created a Chef Cookbooks - NodeJS and NGINX

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source 'https://supermarket.chef.io'
+
+metadata
+
+cookbook 'nodejs', '~> 6.0.0'
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,14 +3,21 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+# This is an example test, replace it with your own test.
+describe service ('nginx') do
+  it {should be_installed}
+  it {should be_enabled}
+  it {should be_running}
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe port(80) do
+  it { should be_listening }
+end
+
+# describe package('nodejs') do
+#   it { should be_installed }
+# end
+
+describe http('http://localhost', enable_remote_worker: true) do
+  its('status'){ should cmp 502 }
 end
